81adb85b008c7fd70cfcd6aa78db315afaf3467d
B12_309_06\b12s309 <jpablohh4@gmail.com>
2024-08-27 10:51:11 -0500

    Cambios

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -4,19 +4,19 @@ class Todo:
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
-        self.completed = False
-        self.tags = []
+        self.completed: bool = False
+        self.tags: list[str] = []
 
     def mark_completed(self):
         self.completed = True
 
-    def add_tag(self, tag):
+    def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
         else:
             print('Elemento repetido')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.code_id} - {self.title}'
 
 prueba = Todo(1, 'hola', 'jaja')
@@ -29,4 +29,9 @@ print(prueba.__str__())
 
 class TodoBook:
     def __init__(self):
-        todos = {}
+        self.todos: dict[int, Todo] = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        todo_id: int = len(self.todos) + 1
+        todo: Todo = Todo(todo_id, title, description)
+        return todo_id
