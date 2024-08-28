e2cd21cfdb51b9b633ca95d2203debd1a16305a3
PC David <hruales1138@gmail.com>
2024-08-27 20:17:06 -0500

    App terminada

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -19,13 +19,6 @@ class Todo:
     def __str__(self) -> str:
         return f'{self.code_id} - {self.title}'
 
-prueba = Todo(1, 'hola', 'jaja')
-print(prueba.tags)
-prueba.add_tag('David')
-print(prueba.tags)
-prueba.add_tag('David')
-print(prueba.tags)
-print(prueba.__str__())
 
 class TodoBook:
     def __init__(self):
@@ -34,4 +27,26 @@ class TodoBook:
     def add_todo(self, title: str, description: str) -> int:
         todo_id: int = len(self.todos) + 1
         todo: Todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = todo
         return todo_id
+
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if todo.completed]
+
+    def tags_todo_count(self) -> dict[str, int]:
+        tags_count: dict[str, int] = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag in tags_count:
+                    tags_count[tag] += 1
+                else:
+                    tags_count[tag] = 1
+        return tags_count
+
+
+
+prueba = Todo(10, 'David', 'jaja')
+print(prueba)
