e91534bfbd7875e58c3051eba7d0df0b65367890
PC David <hruales1138@gmail.com>
2024-08-28 18:42:42 -0500

    App terminada

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,4 +1,4 @@
-# TODO: Add code here
+# 1
 class Todo:
     def __init__(self, code_id: int, title: str, description: str):
         self.code_id: int = code_id
@@ -7,35 +7,41 @@ class Todo:
         self.completed: bool = False
         self.tags: list[str] = []
 
+    # 2
     def mark_completed(self):
         self.completed = True
 
+    # 3
     def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
-        else:
-            print('Elemento repetido')
 
+    # 4
     def __str__(self) -> str:
-        return f'{self.code_id} - {self.title}'
+        return f"{self.code_id} - {self.title}"
 
 
+# 5
 class TodoBook:
     def __init__(self):
         self.todos: dict[int, Todo] = {}
 
+    # 6
     def add_todo(self, title: str, description: str) -> int:
         todo_id: int = len(self.todos) + 1
         todo: Todo = Todo(todo_id, title, description)
         self.todos[todo_id] = todo
         return todo_id
 
+    # 7
     def pending_todos(self) -> list[Todo]:
         return [todo for todo in self.todos.values() if not todo.completed]
 
+    # 8
     def completed_todos(self) -> list[Todo]:
         return [todo for todo in self.todos.values() if todo.completed]
 
+    # 9
     def tags_todo_count(self) -> dict[str, int]:
         tags_count: dict[str, int] = {}
         for todo in self.todos.values():
@@ -45,8 +51,3 @@ class TodoBook:
                 else:
                     tags_count[tag] = 1
         return tags_count
-
-
-
-prueba = Todo(10, 'David', 'jaja')
-print(prueba)
